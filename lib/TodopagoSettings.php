2acae1995d04389d14b1b709594ce9c0e7167720
curcio79 <79460573+curcio79@users.noreply.github.com>
2021-07-26 15:43:43 -0300

    correccion cuotas

--- lib/TodopagoSettings.php
+++ lib/TodopagoSettings.php
@@ -96,15 +96,11 @@ class TodopagoSettings {
 					'type'        => 'select',
 					'description' => 'Puede escojer entre 1 a 12 cuotas',
 					'options'     => array(
+						'24' => 24,
+						'18' => 18,
 						'12' => 12,
-						'11' => 11,
-						'10' => 10,
 						'9'  => 9,
-						'8'  => 8,
-						'7'  => 7,
 						'6'  => 6,
-						'5'  => 5,
-						'4'  => 4,
 						'3'  => 3,
 						'2'  => 2,
 						'1'  => 1
